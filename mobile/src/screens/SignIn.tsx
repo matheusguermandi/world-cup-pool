8d8211e96d71506b7b6a470c2a8f86aff3d8685a
matheusguermandi <matheus_guermandi@hotmail.com>
2022-11-03 15:56:29 +0000

    (mobile) - chore: update screens and styles

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,29 +1,32 @@
-import { Center, Text, Icon } from 'native-base';
-import { Fontisto } from '@expo/vector-icons';
-import { useAuth } from '../hooks/useAuth';
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
 
-import Logo from '../assets/logo.svg';
+import Logo from "../assets/logo.svg";
 
-import { Button } from '../components/Button';
+import { Button } from "../components/Button";
 
 export function SignIn() {
-
-  const { singIn } = useAuth();
+  const { singIn, isUserLoading } = useAuth();
 
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
 
-      <Button 
+      <Button
         type="SECONDARY"
-        title='ENTRAR COM GOOGLE'
-        leftIcon={<Icon as={Fontisto} name='google' color="white" size="md" />}
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
         onPress={singIn}
+        isLoading={isUserLoading}
+        _loading={{
+          _spinner: { color: "white" },
+        }}
       />
 
       <Text color="white" textAlign="center" mt={4}>
-        Não utilizamos nenhuma informação além{'\n'}
+        Não utilizamos nenhuma informação além{"\n"}
         do seu e-mail para criação de sua conta.
       </Text>
     </Center>
